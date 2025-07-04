20671ada90c4822b9932bbbf1e4deb3562a21b4d
Johannes Lohbihler <johannes.lohbihler@gmail.com>
2025-07-04 17:43:36 +0200

    testing bot9

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-//
+BOT9_DATA = {
+    bot9Id: "1e026351-762c-43d9-b3c7-429f1268fae1",
+    // user: {
+    //   userId: '<unique_user_id>',
+    //   emailId: '<user_email>',
+    //   name: '<user_name>',
+    //   customAttributes: {
+    //     Field1: '',
+    //     Field2: '',
+    //     Field3: '',
+    //   }
+    // }
+};
+var chatScript = document.createElement('script');
+chatScript.src = 'https://sdk.chatwidget.in/bot.js';
+document.head.appendChild(chatScript);
